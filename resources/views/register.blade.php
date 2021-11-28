b8bac28b6ec61b37b19e128c05dfeffb86440929
MrWeber94 <abdelkareemelsayed2014@gmail.com>
2021-11-28 18:55:08 +0200

    v11.28

---
+++ resources/views/register.blade.php
@@ -0,0 +1,80 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Register</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <h2>Register</h2>
+  
+  
+  <form   action=""  method="post">
+
+
+  <div class="form-group">
+    <label for="exampleInputName">Name</label>
+    <input type="text" class="form-control" name="name" id="exampleInputName" aria-describedby="" placeholder="Enter Name">
+  </div>
+
+
+  <div class="form-group">
+    <label for="exampleInputEmail">Email address</label>
+    <input type="email"   class="form-control"  name="email" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+  </div>
+
+  <div class="form-group">
+    <label for="exampleInputPassword">New Password</label>
+    <input type="password"   class="form-control" name="password" id="exampleInputPassword1" placeholder="Password">
+  </div>
+ 
+
+  
+  <div class="form-group">
+    <label for="exampleInputPassword">LinkedIn Url</label>
+    <input type="url"   class="form-control" name="linkedIn" id="exampleInputPassword1" placeholder="LinkedIn Url">
+  </div>
+
+
+
+
+
+  <div class="form-group">
+    <label for="exampleInputName">Governorate</label>
+    <input type="text" class="form-control" name="gov" id="exampleInputName" aria-describedby="" placeholder="Enter Governorate">
+  </div>
+
+
+
+  <div class="form-group">
+    <label for="exampleInputName">City</label>
+    <input type="text" class="form-control" name="city" id="exampleInputName" aria-describedby="" placeholder="Enter City">
+  </div>
+
+
+
+  <div class="form-group">
+    <label for="exampleInputName">Address Details</label>
+    <input type="text" class="form-control" name="extraData" id="exampleInputName" aria-describedby="" placeholder="Enter Address Details">
+  </div>
+
+
+
+  
+  <button type="submit" class="btn btn-primary">Save</button>
+</form>
+</div>
+
+</body>
+</html>
+
+
+
+
+
